338ed9fb44d121d956a95c2132854afac0739314
Tyrn <orlov@bos-ltd.ru>
2020-03-21 18:25:36 +0300

    Tabbed MD interface introduced

--- main.py
+++ main.py
@@ -1,77 +1,50 @@
-import kivy
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 
-kivy.require("1.11.1")
-
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
 
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
-
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+class Tab(FloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
 
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
-
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+IS_ICO = False
+TAB_N = 3
 
 
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
+class Example(MDApp):
+    icons = list(md_icons.keys())[15:30]
 
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
-
-    def update(self, dt):
-        self.ball.move()
-
-        # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
-
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.x > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
-
-
-class MainApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+        return Builder.load_file("main_b.kv" if IS_ICO else "main_a.kv")
+
+    def on_start(self):
+        if IS_ICO:
+            for i, name_tab in enumerate(self.icons):
+                if i >= TAB_N: break
+                self.root.ids.android_tabs.add_widget(Tab(text=name_tab))
+        else:
+            for i in range(TAB_N):
+                self.root.ids.android_tabs.add_widget(Tab(text=f"Tab {i}"))
+
+    def on_tab_switch(
+        self, instance_tabs, instance_tab, instance_tab_label, tab_text
+    ):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        if IS_ICO:
+            count_icon = [k for k, v in md_icons.items() if v == tab_text]
+            instance_tab.ids.icon.icon = count_icon[0]
+        else:
+            instance_tab.ids.label.text = tab_text
 
 
-if __name__ == "__main__":
-    MainApp().run()
+Example().run()
