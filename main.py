60c92472610853ee30ea49fdb4962a307701a432
Tyrn <orlov@bos-ltd.ru>
2020-03-23 18:26:51 +0300

    Three different tabs started

--- main.py
+++ main.py
@@ -6,28 +6,26 @@ from kivymd.uix.tab import MDTabsBase
 from kivymd.icon_definitions import md_icons
 
 
-class Tab(FloatLayout, MDTabsBase):
+class TabMain(FloatLayout, MDTabsBase):
     '''Class implementing content for a tab.'''
 
 
-IS_ICO = False
-TAB_N = 3
+class TabList(FloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
+
+
+class TabDetails(FloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
 
 
 class Example(MDApp):
-    icons = list(md_icons.keys())[15:30]
-
     def build(self):
-        return Builder.load_file("main_b.kv" if IS_ICO else "main_a.kv")
+        return Builder.load_file("main_a.kv")
 
     def on_start(self):
-        if IS_ICO:
-            for i, name_tab in enumerate(self.icons):
-                if i >= TAB_N: break
-                self.root.ids.android_tabs.add_widget(Tab(text=name_tab))
-        else:
-            for i in range(TAB_N):
-                self.root.ids.android_tabs.add_widget(Tab(text=f"Tab {i}"))
+        self.root.ids.android_tabs.add_widget(TabMain(text="*TabMain*"))
+        self.root.ids.android_tabs.add_widget(TabList(text="*TabList*"))
+        self.root.ids.android_tabs.add_widget(TabDetails(text="*TabDetails*"))
 
     def on_tab_switch(
         self, instance_tabs, instance_tab, instance_tab_label, tab_text
@@ -40,11 +38,7 @@ class Example(MDApp):
         :param tab_text: text or name icon of tab;
         '''
 
-        if IS_ICO:
-            count_icon = [k for k, v in md_icons.items() if v == tab_text]
-            instance_tab.ids.icon.icon = count_icon[0]
-        else:
-            instance_tab.ids.label.text = tab_text
+        instance_tab.ids.label.text = tab_text
 
 
 Example().run()
