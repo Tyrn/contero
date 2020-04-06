02949c776af339eceb17d5732b32d86957b53cf8
Tyrn <a@b.com>
2020-04-06 15:13:06 +0300

    Moving graph...

--- graphlayout.py
+++ graphlayout.py
@@ -1,31 +1,55 @@
-import sys
-from math import sin
+"""Real time plotting of Microphone level using kivy
+"""
+
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-if len(sys.argv) > 1:
-    from kivy.app import App
-else:
-    from kivymd.app import MDApp as App
-from kivy.uix.widget import Widget
-from kivy_garden.graph import Graph, MeshLinePlot
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy_garden.graph import LinePlot
+from kivy_garden.graph import MeshLinePlot
+from kivy_garden.graph import MeshStemPlot
+from kivy_garden.graph import SmoothLinePlot
+from kivy.clock import Clock
+from math import sin
 
 
-class SetGraph(Widget):
-    def update_graph(self):
-        plot = MeshLinePlot(color=[1, 0, 0, 1])
-        plot.points = [(x, sin(x / 10.0)) for x in range(0, 101)]
-        App.get_running_app().root.ids.graph_test.add_plot(plot)
+POINTS = [(x, sin(x / 1.0) * .3 + .5) for x in range(0, 11)]
 
 
-class GraphLayoutApp(App):
+def shift(points):
+    global POINTS
+    res = []
+
+    for x, y in POINTS:
+        res.append((x + 1.0, y))
+    x, y = res.pop()
+    res.insert(0, (0, y))
+    POINTS = res
+    return res
+
+
+class Logic(BoxLayout):
+    def __init__(self, **kwargs):
+        super(Logic, self).__init__(**kwargs)
+        self.plot = LinePlot(line_width=3, color=[1, 0, 0, 1])
+        #self.plot = MeshLinePlot(color=[1, 0, 0, 1])
+        #self.plot = MeshStemPlot(color=[1, 0, 0, 1])
+        #self.plot = SmoothLinePlot(color=[1, 0, 0, 1])
+
+    def start(self):
+        self.ids.graph.add_plot(self.plot)
+        Clock.schedule_interval(self.get_value, 1.0)
+
+    def stop(self):
+        Clock.unschedule(self.get_value)
+
+    def get_value(self, dt):
+        self.plot.points = shift(POINTS)
+
+
+class RealTimeMicrophone(App):
     def build(self):
         return Builder.load_file("graphlayout.kv")
 
-    def on_start(self):
-        """On Start"""
-        ids = self.root.ids
-        ids.g_test.update_graph()
-
 
 if __name__ == "__main__":
-    GraphLayoutApp().run()
+    RealTimeMicrophone().run()
