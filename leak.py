2a7af1237bec84b5d2e91e52b4657ba174c74e92
Tyrn <orlov@bos-ltd.ru>
2020-04-14 10:43:35 +0300

    No memory leak with Kivy only "list"

--- leak.py
+++ leak.py
@@ -7,44 +7,31 @@ from kivy.utils import platform
 print(f"platform: {platform}")
 
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
 
-from kivymd.app import MDApp
-from kivymd.uix.tab import MDTabsBase
-from kivymd.uix.list import TwoLineAvatarIconListItem
+from kivy.app import App
+from kivy.uix.button import Button
 import gc
 
 
-ACTION_ICON = "eye"
+class PowerListItem(Button):
+    pass
 
 
-class PowerListItem(TwoLineAvatarIconListItem):
-    """The engaged power supply item."""
-
-
-class TabList(FloatLayout, MDTabsBase):
-    """The engaged power supplies tab."""
-
-    def surfacing(self, tab_text):
-        pass
-
-    def discover(self, cnt):
-        ids = MDApp.get_running_app().root.ids
-        ids.ps_toolbar.animate_action_button = False
-        for i in range(cnt):
-            item = PowerListItem(
-                text="List Item" + f" {i + 1:>2}", secondary_text="none"
-            )
-            ids.ps_list.add_widget(item)
-
-
-class Contero(MDApp):
+class Contero(App):
 
     def build(self):
         return Builder.load_file("leak.kv")
 
     def on_start(self):
-        self.theme_cls.primary_palette = "Gray"
+        pass
+
+    def discover(self, cnt):
+        ids = App.get_running_app().root.ids
+        for i in range(cnt):
+            item = PowerListItem(
+                text="List Item" + f" {i + 1:>2}"
+            )
+            ids.ps_list.add_widget(item)
 
     def discovery_clean(self):
         for item in self.root.ids.ps_list.children:
@@ -54,22 +41,9 @@ class Contero(MDApp):
 
     def discovery_request(self, item_count=50):
         self.discovery_clean()
-
-        tab_list = self.root.ids.ps_tab_list
-
-        tab_list.discover(item_count)
-
-    def on_tab_switch(self, instance_tabs, instance_tab, instance_tab_label, tab_text):
-        """Called when switching tabs.
-
-        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
-        :param instance_tab: <__main__.Tab object>;
-        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
-        :param tab_text: text or name icon of tab;
-        """
-
-        instance_tab.surfacing(tab_text)
+        self.discover(item_count)
 
 
 if __name__ == "__main__":
     Contero().run()
+
