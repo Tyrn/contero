ff1c6b629b2c5bc5d7057f6c62de7a9172725a53
Tyrn <orlov@bos-ltd.ru>
2022-03-19 22:40:09 +0300

    Move to PyTest

--- tests/test_contero.py
+++ tests/test_contero.py
@@ -1,19 +1,12 @@
-import unittest
-import co_utils as u
+import contero.co_utils as u
 
 
-class TestUtils(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
+class TestPureFunctions:
     def test_zero_pad(self):
-        self.assertEqual(str(3).zfill(5), "00003")
-        self.assertEqual(str(15331).zfill(3), "15331")
+        assert str(3).zfill(5) == "00003"
+        assert str(15331).zfill(3) == "15331"
 
     def test_mac_int(self):
-        self.assertEqual(u.int_to_mac(0x04_21_3D_34_00_57), "04:21:3d:34:00:57")
-        self.assertEqual(u.mac_to_int("04:21:3d:34:00:57"), 0x04_21_3D_34_00_57)
-        self.assertEqual(u.mac_to_int("ff:ff:ff:ff:ff:ff"), u.MAC_MAX)
+        assert u.int_to_mac(0x04_21_3D_34_00_57) == "04:21:3d:34:00:57"
+        assert u.mac_to_int("04:21:3d:34:00:57") == 0x04_21_3D_34_00_57
+        assert u.mac_to_int("ff:ff:ff:ff:ff:ff") == u.MAC_MAX
