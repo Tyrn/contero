6b3e716122da252f0a52894910662691903783d7
Tyrn <orlov@bos-ltd.ru>
2022-03-21 16:20:40 +0300

    Tab icons added

--- contero/main.py
+++ contero/main.py
@@ -269,9 +269,7 @@ class Contero(MDApp):
             "on_tab_switch",
             destination_tab,
             destination_tab.tab_label,
-            md_icons[
-                "flash" if destination_tab.text == T("co-supplies") else "equalizer"
-            ],
+            destination_tab.text,
         )
         tabs.tab_bar.parent.carousel.load_slide(destination_tab)
 
