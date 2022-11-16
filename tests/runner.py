36659b33ca0e7a352f56f2bbd898ecef2db412d0
Tyrn <a@b.com>
2022-11-16 13:07:50 +0300

    tests/runner.py removed

--- tests/runner.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import os
-
-loader = unittest.TestLoader()
-print(f'Discovery in "{os.getcwd()}"')
-tests = loader.discover(".")
-testRunner = unittest.runner.TextTestRunner()
-testRunner.run(tests)
